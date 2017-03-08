f003b49fe408ebd4d4208fc732e9e05a62c17d01
Barłomiej Słowik <slowikbartek@gmail.com>
2017-03-08 18:54:34 +0100

    Switched from interface to class and added methods.

--- src/main/java/algorithms/Algorithm.java
+++ src/main/java/algorithms/Algorithm.java
@@ -1,6 +1,21 @@
 package algorithms;
 
-public interface Algorithm {
+import process.Process;
 
-    int[] run();
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Algorithm {
+
+    protected Queue<Process> processQueue;
+    protected boolean isDone;
+
+    public Algorithm(boolean isDone) {
+        this.processQueue = new LinkedList<>();
+        this.isDone = isDone;
+    }
+
+    public void addProcess(Process process) {
+        processQueue.add(process);
+    }
 }
